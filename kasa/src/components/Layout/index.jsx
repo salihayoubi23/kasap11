0fd21d74fea58254db4521a9877f09992d64a23c
saliha youbi <salihayoubi23@outlook.com>
2023-11-14 15:22:18 +0100

    mise en page footer v smartphone + comments

--- kasa/src/components/Layout/index.jsx
+++ kasa/src/components/Layout/index.jsx
@@ -1,25 +1,37 @@
+// Importation du fichier de style CSS pour appliquer les styles spécifiques à ce composant.
 import './style.css';
+
+// Importation des composants nécessaires depuis react-router-dom.
 import { Link, NavLink, Outlet } from 'react-router-dom';
+
+// Importation des logos depuis le répertoire des assets.
 import kasaHeaderLogo from '../../assets/LOGO.svg';
 import kasaFooterLogo from '../../assets/LOGO_WHITE.svg';
 
 /************************************************/
-//*  composant Layout                           */
+//*  Composant Layout                           */
 /************************************************/
 
+// Définition du composant fonctionnel Layout.
 const Layout = () => (
+  // Le contenu du composant est entouré par une div avec la classe 'layoutWrapper'.
   <div className='layoutWrapper'>
+    {/* En-tête de la page */}
     <header>
+      {/* Lien vers la page d'accueil avec le logo de l'en-tête */}
       <Link to='/'>
         <img className='logo' src={kasaHeaderLogo} alt='logo de kasa' />
       </Link>
+      {/* Barre de navigation */}
       <nav>
         <ul className='menu'>
+          {/* Lien de navigation vers la page d'accueil */}
           <li>
             <NavLink className='menuItem' to='/'>
               Accueil
             </NavLink>
           </li>
+          {/* Lien de navigation vers la page "A Propos" */}
           <li>
             <NavLink className='menuItem' to='about'>
               A Propos
@@ -28,16 +40,24 @@ const Layout = () => (
         </ul>
       </nav>
     </header>
+
+    {/* Section principale */}
     <main>
+      {/* Outlet pour afficher le contenu de la route parente */}
       <Outlet />
     </main>
+
+    {/* Pied de page */}
     <footer>
+      {/* Lien vers la page d'accueil avec le logo du pied de page */}
       <Link to='/'>
         <img className='logo' src={kasaFooterLogo} alt='logo de kasa' />
       </Link>
+      {/* Texte du copyright */}
       <span>© 2020 Kasa. All rights reserved</span>
     </footer>
   </div>
 );
 
+// Export du composant Layout pour une utilisation dans d'autres parties de l'application.
 export default Layout;
